18327c85a4f7b974aa2ff3b7aa529a5c307b5cc8
NaCuMa <83917817+NaCuMa@users.noreply.github.com>
2022-09-02 09:53:37 +0200

    Add files via upload

--- Files to replace/For TADDY/0_prepare_data.py
+++ Files to replace/For TADDY/0_prepare_data.py
@@ -12,7 +12,7 @@ def preprocessDataset(dataset):
     t0 = time.time()
     if dataset in ['digg', 'uci', 'AST']:
         edges = np.loadtxt(
-            'C:/Users/geode/TADDY/data/raw/' +
+            '../TADDY/data/raw/' +
             dataset,
             dtype=float,
             comments='%',
@@ -23,13 +23,13 @@ def preprocessDataset(dataset):
         edges = edges[:, 0:2].astype(dtype=int)
     elif dataset in ['btc_alpha', 'btc_otc', 'email', 'TGN']:
         if dataset == 'btc_alpha':
-            file_name = 'C:/Users/geode/TADDY/data/raw/' + 'soc-sign-bitcoinalpha.csv'
+            file_name = '../TADDY/data/raw/' + 'soc-sign-bitcoinalpha.csv'
         elif dataset =='btc_otc':
-            file_name = 'C:/Users/geode/TADDY/data/raw/' + 'soc-sign-bitcoinotc.csv'
+            file_name = '../TADDY/data/raw/' + 'soc-sign-bitcoinotc.csv'
         elif dataset =='email':
-            file_name = 'C:/Users/geode/TADDY/data/raw/' + 'email-dnc.csv'
+            file_name = '../TADDY/data/raw/' + 'email-dnc.csv'
         elif dataset =='TGN':
-            file_name = 'C:/Users/geode/TADDY/data/raw/' + 'TGN.csv'
+            file_name = '../TADDY/data/raw/' + 'TGN.csv'
         with open(file_name) as f:
             lines = f.read().splitlines()
         edges = [[float(r) for r in row.split(',')] for row in lines]
@@ -55,14 +55,15 @@ def preprocessDataset(dataset):
     print('vertex:', len(vertexs), ' edge: ', len(edges))
 # On enregistre times pour permettre de faire le lien entre les données traitées et non traitées
 # vertexs permet de faire le lien entre les indices traités et non traités
-    Path('C:/Users/geode/TADDY/data/for_TGN/').mkdir(parents=True, exist_ok=True)
-    pd.DataFrame(times).to_csv(f'C:/Users/geode/TADDY/data/for_TGN/' + dataset + '_times.csv',
+    Path('../TADDY/data/for_TGN/').mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(times).to_csv(f'../TADDY/data/for_TGN/' + dataset + '_times.csv',
                                        index=False)
-    pd.DataFrame(vertexs).to_csv(f'C:/Users/geode/TADDY/data/for_TGN/' + dataset + '_vertexs.csv',
+    pd.DataFrame(vertexs).to_csv(f'../TADDY/data/for_TGN/' + dataset + '_vertexs.csv',
                                        index=False)
 #----------------------------------------------------------------
+    Path('../TADDY/data/interim/').mkdir(parents=True, exist_ok=True)
     np.savetxt(
-        'C:/Users/geode/TADDY/data/interim/' +
+        '../TADDY/data/interim/' +
         dataset,
         X=edges,
         delimiter=' ',
@@ -73,10 +74,10 @@ def preprocessDataset(dataset):
 
 def generateDataset(dataset, snap_size, train_per=0.5, anomaly_per=0.01):
     print('Generating data with anomaly for Dataset: ', dataset)
-    if not os.path.exists('C:/Users/geode/TADDY/data/interim/' + dataset):
+    if not os.path.exists('../TADDY/data/interim/' + dataset):
         preprocessDataset(dataset)
     edges = np.loadtxt(
-        'C:/Users/geode/TADDY/data/interim/' +
+        '../TADDY/data/interim/' +
         dataset,
         dtype=float,
         comments='%',
@@ -93,10 +94,10 @@ def generateDataset(dataset, snap_size, train_per=0.5, anomaly_per=0.01):
     synthetic_test, train_mat, train = anomaly_generation(train_per, anomaly_per, edges, n, m, seed=1, tgn = tgn)
 # Récupérer les données traitées pour TADDY permet soit de les utiliser directement pour entraîner TGN (mauvaise idée),
 # soit de reconstruire un jeu de données comparable à celui utilisé par TADDY mais adapté à TGN
-    Path('C:/Users/geode/TADDY/data/for_TGN/').mkdir(parents=True, exist_ok=True)
-    pd.DataFrame(train).to_csv(f'C:/Users/geode/TADDY/data/for_TGN/' + 'train_' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) +'.csv',
+    Path('../TADDY/data/for_TGN/').mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(train).to_csv(f'../TADDY/data/for_TGN/' + 'train_' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) +'.csv',
                                        index=False)
-    pd.DataFrame(synthetic_test).to_csv(f'C:/Users/geode/TADDY/data/for_TGN/' + 'test_' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) +'.csv',
+    pd.DataFrame(synthetic_test).to_csv(f'../TADDY/data/for_TGN/' + 'test_' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) +'.csv',
                                        index=False)
 #------------------------------------------------------------------
     print("Anomaly Generation finish! Time: %.2f s"%(time.time()-t0))
@@ -153,12 +154,12 @@ def generateDataset(dataset, snap_size, train_per=0.5, anomaly_per=0.01):
 # Disposer des indices de snaps permet de retrouver les instants correspondants pour l'évaluation des aucs.
 # Cela permet également de déterminer l'instant jusqu'auquel entraîner TGN.
     snap_list.append(synthetic_test.shape[0])
-    Path('C:/Users/geode/TADDY/data/for_TGN/').mkdir(parents=True, exist_ok=True)
-    pd.DataFrame(snap_list).to_csv(f'C:/Users/geode/TADDY/data/for_TGN/' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) +'_snap_indices.csv',
+    Path('../TADDY/data/for_TGN/').mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(snap_list).to_csv(f'../TADDY/data/for_TGN/' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) +'_snap_indices.csv',
                                        index=False)
 #------------------------------------------------------------------
-
-    with open('C:/Users/geode/TADDY/data/percent/' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) + '.pkl', 'wb') as f:
+    Path('../TADDY/data/percent/').mkdir(parents=True, exist_ok=True)
+    with open('../TADDY/data/percent/' + dataset + '_' + str(train_per) + '_' + str(anomaly_per) + '.pkl', 'wb') as f:
         pickle.dump((rows,cols,labs,weis,headtail,train_size,test_size,n,m),f,pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
